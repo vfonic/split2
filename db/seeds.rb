5d703f503c12577cf313e7c0c020f4e988f53386
Viktor Fonic <viktor.fonic@gmail.com>
2017-01-05 16:18:30 +0700

    rails_apps_composer: set up database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+user = CreateAdminService.new.call
+puts 'CREATED ADMIN USER: ' << user.email

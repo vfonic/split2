3cac6698d2ddd67aba0de4ebed6a160a5bed4a34
Viktor Fonic <viktor.fonic@gmail.com>
2017-01-05 23:51:34 +0700

    Annotate CollectionProduct

--- app/models/collection_product.rb
+++ app/models/collection_product.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: collection_products
+#
+#  id            :integer          not null, primary key
+#  collection_id :integer
+#  product_id    :integer
+#
+# Indexes
+#
+#  index_collection_products_on_collection_id  (collection_id)
+#  index_collection_products_on_product_id     (product_id)
+#
+
 class CollectionProduct < ActiveRecord::Base
   belongs_to :collection
   belongs_to :product

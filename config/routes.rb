0403f04ceaaa104fed006dfa790143fb60bc1225
Viktor Fonic <viktor.fonic@gmail.com>
2017-01-05 21:47:48 +0700

    Comment out non-existing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
-  devise_for :users
-  resources :users
+  # root to: 'visitors#index'
+  # devise_for :users
   mount MailPreview => 'mail_view' if Rails.env.development?
 end

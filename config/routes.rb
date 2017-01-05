07c91b457b75f7da7da21ecddea0666ce8ea2b2b
Viktor Fonic <viktor.fonic@gmail.com>
2017-01-05 23:05:12 +0700

    Annotate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
+# == Route Map
+#
+#       Prefix Verb URI Pattern Controller#Action
+# mail_preview      /mail_view  MailPreview
+#         page GET  /pages/*id  high_voltage/pages#show
+#
+
 Rails.application.routes.draw do
   # root to: 'visitors#index'
   # devise_for :users

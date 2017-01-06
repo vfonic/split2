7c4e228f8d12dbdb62a6db2164159a71da36d342
Viktor Fonic <viktor.fonic@gmail.com>
2017-01-06 20:12:33 +0700

    Use file url instead of file path

--- app/filters/url_filters.rb
+++ app/filters/url_filters.rb
@@ -2,7 +2,7 @@ module UrlFilters
   include ApplicationHelper
 
   def asset_url(asset_name)
-    asset_url_for_key("assets/#{asset_name}").file.path
+    asset_url_for_key("assets/#{asset_name}").file.url
   end
 
   def stylesheet_tag(stylesheet_path)
